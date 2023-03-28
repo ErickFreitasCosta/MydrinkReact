644a713b126fccd420a788ce580d8c4bb3a65fe3
ErickFreitasCosta <erickifpa2021@gmail.com>
2023-03-28 10:45:36 -0300

    6

--- src/Componentes/Navbar/navbar.js
+++ src/Componentes/Navbar/navbar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import TypesExample from '../botao/botao';
-
+import { Link } from 'react-router-dom';
 // img
 import Logo from '../../img/glass.svg';
 
@@ -15,7 +15,7 @@ function CollapsibleExample() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className='logo' >
           <Nav className="me-auto" id='logo'>
-            <Nav.Link href="fale" id='logo'>Contatos</Nav.Link>
+            <Nav.Link as={Link} to="/fale" id='logo'>Contatos</Nav.Link>
             <NavDropdown title="Produtos" id="logo">
               <NavDropdown.Item href="#action/3.1" >Whisky</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
